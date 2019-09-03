0bd932c37b020cad776d338ab33d71ec4d9a6ee8
Caio Rodrigues <rfcaio90@gmail.com>
2019-09-03 15:29:01 -0300

    jquery-aula-02

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,15 @@
 (function () {
   'use strict';
 
-  var message = $('#message').text();
-  var messageLength = message.split(' ').length;
-  $('#message-length').text(messageLength === 1 ? 'A single word.' : messageLength + ' words.');
+  var isNonEmptyString = function isNonEmptyString (word) {
+    return word !== '';
+  };
+
+  $('#game-phrase-word-amount').text($('#game-phrase').text().split(/\s+/).length);
+
+  var input = $('#game-input');
+  input.on('input', function onInput () {
+    $('#game-input-length').text(input.val().length);
+    $('#game-input-word-amount').text(input.val().split(/\s+/).filter(isNonEmptyString).length);
+  });
 }());
