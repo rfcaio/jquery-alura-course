be652a0dea89742f882ae5aa2f9b932a0dceb158
Caio Rodrigues <rfcaio90@gmail.com>
2019-09-04 11:41:47 -0300

    jquery-aula-03

--- src/js/app.js
+++ src/js/app.js
@@ -13,4 +13,15 @@
     $('#game-input-length').text(input.val().length);
     $('#game-input-word-amount').text(input.val().split(/\s+/).filter(isNonEmptyString).length);
   });
+
+  input.one('focus', function onFocus () {
+    var gameInterval = setInterval(function tick () {
+      var time = parseInt($('#game-time').text(), 10);
+      if (time <= 1) {
+        clearInterval(gameInterval);
+        input.attr('disabled', true);
+      }
+      $('#game-time').text(time - 1);
+    }, 1000);
+  });
 }());
