3dc55ec06382059bbd21c44e12804bec07d5d147
Caio Rodrigues <rfcaio90@gmail.com>
2019-09-05 14:17:49 -0300

    jquery-aula-04

--- src/js/app.js
+++ src/js/app.js
@@ -2,26 +2,49 @@
 (function () {
   'use strict';
 
-  var isNonEmptyString = function isNonEmptyString (word) {
-    return word !== '';
+  var setClockUpdate = function setClockUpdate () {
+    var gameInput = $('#game-input');
+
+    gameInput.one('focus', function onGameInputFocus () {
+      var gameInterval = setInterval(function tick () {
+        var time = parseInt($('#game-time').text(), 10);
+        if (time <= 1) {
+          $('#game-restart-button').removeAttr('disabled');
+          clearInterval(gameInterval);
+          gameInput.attr('disabled', true);
+        }
+        $('#game-time').text(time - 1);
+      }, 1000);
+    });
   };
 
-  $('#game-phrase-word-amount').text($('#game-phrase').text().split(/\s+/).length);
+  var setGameRestart = function setGameRestart () {
+    $('#game-input').removeAttr('disabled').val('');
+    $('#game-input-length').text(0);
+    $('#game-input-word-amount').text(0);
+    $('#game-restart-button').attr('disabled', true);
+    $('#game-time').text(10);
+    setClockUpdate();
+  };
 
-  var input = $('#game-input');
-  input.on('input', function onInput () {
-    $('#game-input-length').text(input.val().length);
-    $('#game-input-word-amount').text(input.val().split(/\s+/).filter(isNonEmptyString).length);
-  });
+  var setScoreUpdate = function setScoreUpdate () {
+    var gameInput = $('#game-input');
+    var isNonEmptyString = function isNonEmptyString (word) {
+      return word !== '';
+    };
 
-  input.one('focus', function onFocus () {
-    var gameInterval = setInterval(function tick () {
-      var time = parseInt($('#game-time').text(), 10);
-      if (time <= 1) {
-        clearInterval(gameInterval);
-        input.attr('disabled', true);
-      }
-      $('#game-time').text(time - 1);
-    }, 1000);
-  });
+    gameInput.on('input', function onGameInputChange () {
+      $('#game-input-length').text(gameInput.val().length);
+      $('#game-input-word-amount').text(gameInput.val().split(/\s+/).filter(isNonEmptyString).length);
+    });
+  };
+
+  var startGame = function startGame () {
+    $('#game-phrase-word-amount').text($('#game-phrase').text().split(/\s+/).length);
+    $('#game-restart-button').click(setGameRestart);
+    setScoreUpdate();
+    setClockUpdate();
+  };
+
+  $(document).ready(startGame);
 }());
